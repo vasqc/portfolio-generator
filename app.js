9e51476c5edda83a589e1b3b181dd73a5e257006
vasqc <90007988+vasqc@users.noreply.github.com>
2021-12-23 17:33:21 -0500

    node

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-//const generatePage = () => 'Name: Jane, Github: janehub';
-//const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const generatePage = (userName, githubName) => {
-    return `
-      Name: ${userName}
-      GitHub: ${githubName}
-    `;
-};
-console.log(name,github);
-console.log(generatePage(name, github));
-//console.log(generatePage('Jane', 'janehub'));
+const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
