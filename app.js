d4dc8befc4b1f462aacb341b4db416b73046a468
vasqc <90007988+vasqc@users.noreply.github.com>
2021-12-26 12:53:22 -0500

    inquirer

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
+
+promptUser().then(answers => console.log(answers));
